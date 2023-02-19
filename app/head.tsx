cd4209737abe1be963d0825ac3892b96c43fc603
vinewz <vinewalves@hotmail.com>
2023-02-19 20:23:21 -0300

    Remove meta tags breaking indexing on social media

--- app/head.tsx
+++ app/head.tsx
@@ -1,11 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Vinicios Silva</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
-      <meta name="author" content="Vinicios Silva"/>
-      <meta name="keywords" content="html, css, javascript, react, reactjs, next, nextjs, typescript, developer, front-end"/>
       <link rel="apple-touch-icon" sizes="180x180" href="https://www.vinicios.tk/assets/logo-fav/apple-touch-icon.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="https://www.vinicios.tk/assets/logo-fav/favicon-32x32.png"/>
       <link rel="icon" type="image/png" sizes="16x16" href="https://www.vinicios.tk/assets/logo-fav/favicon-16x16.png"/>
