560774cb58efeace2b9dc085d8adb79d6ea907b8
vinewz <vinewalves@hotmail.com>
2023-02-19 17:56:11 -0300

    Change home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import shiki from 'shiki'
+
+
+const markdown: string = `
+  # Purpose
+
+  Project created with a purpose to study Next 13 new features and to store some of my
+  useful config files so i can get them from any pc that i log-in.
+`.trim()
+
+
+
+export default async function Home() {
+
+  const highlighter = await shiki.getHighlighter({
+    theme: 'rose-pine-moon'
+  })
+
+  const code = highlighter.codeToHtml(markdown, {lang: 'md'})
+
   return (
-    <div className="">
-      home
-    </div>
+    <div dangerouslySetInnerHTML={{__html: code}} />
   )
 }
